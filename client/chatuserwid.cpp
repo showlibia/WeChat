39def1fb9c4f30cccb69e1788c86c2b0dab5a889
showbilia <frunnever@gmail.com>
2025-02-23 20:42:33 +0800

    feat(client): change the end of a long message to an ellipsis

--- client/chatuserwid.cpp
+++ client/chatuserwid.cpp
@@ -24,6 +24,16 @@ void ChatUserWid::SetInfo(QString name, QString head, QString msg)
     // 设置图片自动缩放
     ui->icon_lb->setPixmap(pixmap.scaled(ui->icon_lb->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
     ui->icon_lb->setScaledContents(true);
-    ui->user_name_lb->setText(_name);
-    ui->user_chat_lb->setText(_msg);
+
+    // 设置用户名显示省略号
+    QFontMetrics nameFontMetrics(ui->user_name_lb->font());
+    int nameWidth = ui->user_name_lb->width(); // 获取 QLabel 的宽度
+    QString elidedName = nameFontMetrics.elidedText(_name, Qt::ElideRight, nameWidth);
+
+    QFontMetrics msgFontMetrics(ui->user_chat_lb->font());
+    int msgWidth = ui->user_chat_lb->width(); // 获取 QLabel 的宽度
+    QString elidedMsg = msgFontMetrics.elidedText(_msg, Qt::ElideRight, msgWidth);
+
+    ui->user_name_lb->setText(elidedName);
+    ui->user_chat_lb->setText(elidedMsg);
 }
